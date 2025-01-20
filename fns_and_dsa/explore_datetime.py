f996d91ae526414ffabdb29b2366f66a4c9367ec
Abdi Yizotaw <abdiyizotaw12@gmail.com>
2025-01-20 20:45:15 +0300

    changed the explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,50 +1,16 @@
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
-        self.__is_checked_out = False
-
-    def check_out(self):
-        if not self.__is_checked_out:
-            self.__is_checked_out = True
-            return True
-        return False
-    
-    def return_book(self):
-        if self.__is_checked_out:
-            self.__is_checked_out = False
-            return True
-        return False
-    
-    def is_available(self):
-        return not self.__is_checked_out
+from datetime import datetime
+from datetime import timedelta
 
 
-class Library:
-    def __init__(self):
-        self._books = []
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f"Current date and time: {current_date:%Y-%m-%d %H:%M:%S}") 
+display_current_datetime()
 
-    def add_book(self, book):
-        if isinstance(book, Book):
-            self._books.append(book)
-
-    def check_out_book(self, title):
-        for book in self._books:
-            if book.title == title and book.is_available():
-                if book.check_out():
-                    return f'You have checked out "{title}".'
-        return f'"{title}" is not available.'
-
-    def return_book(self, title):
-        for book in self._books:
-            if book.title == title and not book.is_available():
-                if book.return_book():
-                    return f'You have returned "{title}".'
-        return f'"{title}" was not checked out.'
-
-
-    def list_available_books(self):
-        available_books = [book.title for book in self._books if book.is_available()]
-        return available_books
-
-    
+number_of_days = int(input("Enter the number of days to add to the current date: "))
+def calculate_future_date():
+    current_date = datetime.now()
+    delta_days = timedelta(days=number_of_days) 
+    future_date = current_date + delta_days
+    print(f"future date: {future_date.strftime('%Y-%m-%d')}")
+calculate_future_date()
