5de7923e71385039629f6ea56ee0df99682d1e7f
Abdi Yizotaw <abdiyizotaw12@gmail.com>
2025-01-26 22:10:03 +0300

    modified

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,7 +2,8 @@ class Book:
     def __init__(self, title: str, author:str, year:int):
         self.title = title
         self.author = author
-        self.year = year
+        self.year = year 
+        
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
